25d286a789621a558bdf6f2ff5709fd6a925ad50
Vlad Grytsenko <vladik.grytsenko2017@gmail.com>
2019-09-19 19:44:55 +0300

    Updates

---
+++ precache-manifest.2e83a361a9d2421d35937ef55377c0ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c27008debd1a98768c7f6447bdadf9",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "a3f998eefa65fd45e7cd",
+    "url": "/react_add-todo-form/static/css/main.f50536bc.chunk.css"
+  },
+  {
+    "revision": "6c8f1d725de506c0f583",
+    "url": "/react_add-todo-form/static/js/2.e8aa7888.chunk.js"
+  },
+  {
+    "revision": "a3f998eefa65fd45e7cd",
+    "url": "/react_add-todo-form/static/js/main.4faf5dfe.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
